ae18609c1ad1e11e3dd0e2b417238f3aa961decd
WizAnamay <67374665+wizanamay@users.noreply.github.com>
2020-06-24 19:04:14 +0530

    1) Main.py
    Simple Scientific Calculator is created by python. This is the main.py file. While other Files you have to import one-by-one.

---
+++ main.py
@@ -0,0 +1,24 @@
+#Take value from user for operation
+print("What do you want to do from above operation?('Enter the number for operation selelction')")
+print("1)Simple Calculator:Addition,Subtraction,Multiplication,Division")
+print("2)To find determinent")
+print("3)To find Slope")
+print("4)To find Addition of two vector")
+print("5)Joint Equation of pair of line")
+print("6) To find greates number of two")
+lol=int(input())
+if lol==1:
+  import simple.py
+elif lol==2:
+  import deter.py
+elif lol==3:
+  import slope.py
+elif lol==4:
+  import vector.py
+elif lol==5:
+  import equa.py   
+elif lol==6:
+  import great.py  
+else:
+  print("INVALID ENTERY.") 
+
