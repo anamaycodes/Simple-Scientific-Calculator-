8f4300847189116c105db3f63f8aa26d55aefd73
WizAnamay <67374665+wizanamay@users.noreply.github.com>
2020-06-24 19:23:50 +0530

    Create 4)great.py

---
+++ 4)great.py
@@ -0,0 +1,8 @@
+list1=[]
+num=int(input("Entered the elements of number in the list "))
+for i in range(1,num+1):
+  lol=int(input("Enter the numbers"))
+  list1.append(lol)
+print("The numbers you have entered is",list1)
+print("Largest number is ",max(list1)) 
+exit()
